c08f4c7da88ba435d95ee6a87cca6c1d010c7eae
Olagundoye T. Niyi <teejaybloom@yahoo.com>
2025-02-06 11:04:25 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "aws_security_group" "demotf_security_group" {
 variable "instance_keypair" {
   description = "AWS EC2 key pair for ssh access"
   type = string
-  default ="tutorkp"
+  default ="niyikp"
   sensitive = true
 }
 
